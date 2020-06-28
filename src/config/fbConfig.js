b0d5210184dc8fefa19b933249ad3e45b4eb8938
Natalie <natalie@Natalies-MacBook-Air.local>
2020-06-28 23:29:36 +0200

    change fb config

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,13 +3,13 @@
   import 'firebase/auth'
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyCH5Xc_oikeC9fE-XDpwzL2SfqhwWfMb4g",
-    authDomain: "recipe-site-68898.firebaseapp.com",
-    databaseURL: "https://recipe-site-68898.firebaseio.com",
-    projectId: "recipe-site-68898",
-    storageBucket: "",
-    messagingSenderId: "1081676366277",
-    appId: "1:1081676366277:web:f49ac88c4f15ab2b"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
