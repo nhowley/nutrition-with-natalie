1dd3e52ec31ffde8b91b2d088ae8be6fd01500d8
Natalie <natalie@Natalies-MacBook-Air.local>
2020-06-28 23:20:47 +0200

    remove port

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Our app is running on port ${ PORT }`);
+// });
