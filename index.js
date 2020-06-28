bacbd441940ec7f375e4320e0d9a00f1169625b5
Natalie <natalie@Natalies-MacBook-Air.local>
2020-06-28 23:15:11 +0200

    change port

--- index.js
+++ index.js
@@ -55,4 +55,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-app.listen(3002)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
