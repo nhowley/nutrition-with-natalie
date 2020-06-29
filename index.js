e9ba30fef405d5f30413877d51ef775e59c19606
Natalie <natalie@Natalies-MacBook-Air.local>
2020-06-29 19:43:53 +0200

    edit port

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ app.use(cors())
 app.use(express.json())
 app.use('/', router)
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode");
 });
