63062037237d9192bc879e809625a84d6d636565
Natalie <natalie@Natalies-MacBook-Air.local>
2020-06-29 19:53:20 +0200

    add port

--- index.js
+++ index.js
@@ -55,6 +55,4 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode");
-});
+app.listen(process.env.PORT)
